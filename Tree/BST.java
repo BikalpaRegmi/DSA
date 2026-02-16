2da85cccb382d8b86be7157c5a16314a59ee13c6
Bikalpa Regmi <bikalparegmihtd3@gmail.com>
2026-02-16 11:07:32 +0545

    Binary Search Tree

---
+++ Tree/BST.java
@@ -0,0 +1,119 @@
+public class BST{
+public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+
+        System.out.println("Inorder traversal of the given tree:");
+        tree.inorder();   
+
+        tree.deleteKey(20);
+
+        tree.deleteKey(30);
+
+        tree.deleteKey(50);
+        
+        System.out.println("Inorder after deleting 20,30,50:");
+        tree.inorder();   
+    }
+}
+
+
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+class BinarySearchTree {
+    Node root;
+
+    BinarySearchTree() {
+        root = null;
+    }
+
+    public void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    private Node insertRec(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        if (key < root.data) {
+            root.left = insertRec(root.left, key);
+        } else if (key > root.data) {
+            root.right = insertRec(root.right, key);
+        }
+        return root;
+    }
+
+
+    public void deleteKey(int key) {
+        root = deleteRec(root, key);
+    }
+
+    private Node deleteRec(Node root, int key) {
+        if (root == null) {
+            return root;
+        }
+
+        if (key < root.data) {
+            root.left = deleteRec(root.left, key);
+        } else if (key > root.data) {
+            root.right = deleteRec(root.right, key);
+        }
+
+        else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            root.data = minValue(root.right);
+
+            root.right = deleteRec(root.right, root.data);
+        }
+
+        return root;
+    }
+
+    private int minValue(Node root) {
+        int minv = root.data;
+        while (root.left != null) {
+            minv = root.left.data;
+            root = root.left;
+        }
+        return minv;
+    }
+
+    public void inorder() {
+        inorderRec(root);
+        System.out.println();
+    }
+
+    private void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.data + " ");
+            inorderRec(root.right);
+        }
+    
+}
+}
+
+    
